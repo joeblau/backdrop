dc5f58a0999c8d54069def01b0f5e4c22e48568a
Joe Blau <im@joeblau.com>
2021-07-01 15:34:13 -0700

    Initial Commit

---
+++ Shared/BackdropApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackdropApp.swift
+//  Shared
+//
+//  Created by Joe Blau on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BackdropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
